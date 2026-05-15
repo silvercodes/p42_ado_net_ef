59d13c1324048eaa7fbb53c7529e7d97e6c2bcb9
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-05-15 23:45:12 +0300

    add:prepared queries

--- 01_simple_ado_net/02_sp/Program.cs
+++ 01_simple_ado_net/02_sp/Program.cs
@@ -1,17 +1,64 @@
 ﻿
-
-
+using System.Data;
 using Microsoft.Data.SqlClient;
 
 const string connString = @"Server=.\SQLEXPRESS;Database=p42_insta_db;Trusted_Connection=True;Encrypt=False;";
 
+
+
+
+//string procQuery = @"
+//    CREATE PROCEDURE uspGetUserOf2000
+//    AS
+//    BEGIN
+//        SELECT id, email
+//        FROM users
+//        WHERE YEAR(birthday) = 2000;
+//    END
+//";
+
+//using SqlConnection conn = new SqlConnection(connString);
+
+//try
+//{
+//    conn.Open();
+
+//    Console.WriteLine("Connection OK");
+
+//    //SqlCommand cmd = new SqlCommand(procQuery, conn);
+//    //cmd.ExecuteNonQuery();
+
+//    SqlDataReader reader = GetProcReader("uspGetUserOf2000");
+//    RenderResult(reader);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"ERROR: {ex.Message}");
+//}
+//finally
+//{
+//    if (conn.State == System.Data.ConnectionState.Open)
+//    {
+//        conn.Close();
+//        Console.WriteLine("Connection closed");
+//    }
+//}
+
+
+
+
+
 string procQuery = @"
-    CREATE PROCEDURE uspGetUserOf2000
+    CREATE PROCEDURE uspGetUsersCountByEmailPattern
+        @pattern nvarchar(50),
+        @count int OUT
     AS
     BEGIN
-        SELECT id, email
-        FROM users
-        WHERE YEAR(birthday) = 2000;
+        SET @count = (
+            SELECT COUNT(email)
+            FROM users
+            WHERE email LIKE @pattern
+        );
     END
 ";
 
@@ -23,10 +70,11 @@ try
 
     Console.WriteLine("Connection OK");
 
-    SqlCommand cmd = new SqlCommand(procQuery, conn);
-    cmd.ExecuteNonQuery();
+    //SqlCommand cmd = new SqlCommand(procQuery, conn);
+    //cmd.ExecuteNonQuery();
 
-
+    int result = CountByEmail("a%");
+    Console.WriteLine($"Count = {result}");
 }
 catch (Exception ex)
 {
@@ -40,3 +88,77 @@ finally
         Console.WriteLine("Connection closed");
     }
 }
+
+int CountByEmail(string pattern)
+{
+    string procName = "uspGetUsersCountByEmailPattern";
+
+    SqlCommand cmd = new SqlCommand(procName, conn)
+    {
+        CommandType = CommandType.StoredProcedure,
+    };
+
+    cmd.Parameters.Add(new SqlParameter()
+    {
+        ParameterName = "@pattern",
+        SqlDbType = SqlDbType.NVarChar,
+        Size = 50,
+        Value = pattern,
+    });
+
+    SqlParameter countPrm = new SqlParameter()
+    {
+        ParameterName = "@count",
+        SqlDbType = SqlDbType.Int,
+        Direction = ParameterDirection.Output,
+    };
+
+    cmd.Parameters.Add(countPrm);
+
+    cmd.ExecuteNonQuery();
+
+    return (int)countPrm.Value;
+}
+
+
+
+SqlDataReader GetProcReader(string procName)
+{
+    SqlCommand cmd = new SqlCommand()
+    {
+        Connection = conn,
+        CommandType = System.Data.CommandType.StoredProcedure,      // <<< !!!
+        CommandText = procName
+    };
+
+    return cmd.ExecuteReader();
+}
+
+void RenderResult(SqlDataReader reader)
+{
+    DataTable dt = new DataTable();
+    dt.Load(reader);
+
+    int columnsCount = dt.Columns.Count;
+
+    Console.Write($"#\t\t");
+    foreach(DataColumn col in dt.Columns)
+        Console.Write($"{col.ColumnName}\t\t");
+    Console.WriteLine();
+
+    for(int i = 0; i < dt.Rows.Count; ++i)
+    {
+        Console.Write($"{i + 1}\t\t");
+
+        DataRow row = dt.Rows[i];
+
+        for (int j = 0; j < columnsCount; j++)
+        {
+            Console.Write($"{row[j]}\t\t");
+        }
+
+        Console.WriteLine();
+    }
+    
+}
+
---
+++ 01_simple_ado_net/03_prepared_query/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+const string connString = @"Server=.\SQLEXPRESS;Database=p42_logbook_db;Trusted_Connection=True;Encrypt=False;";
+
+//using SqlConnection conn = new SqlConnection(connString);
+
+//// string title = "C++";
+
+//string title = "my_title');INSERT INTO subjects (title) VALUES('ADMIN LOOSER!!!";
+
+//try
+//{
+//    conn.Open();
+//    Console.WriteLine("Connection OK");
+
+//    string query = $@"INSERT INTO subjects (title) VALUES ('{title}')";
+//    Console.WriteLine(query);
+
+//    SqlCommand cmd = new SqlCommand(query, conn);
+//    cmd.ExecuteNonQuery();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"ERROR: {ex.Message}");
+//}
+//finally
+//{
+//    if (conn.State == System.Data.ConnectionState.Open)
+//    {
+//        conn.Close();
+//        Console.WriteLine("Connection closed");
+//    }
+//}
+
+
+
+
+
+
+
+
+//using SqlConnection conn = new SqlConnection(connString);
+
+//// string title = "C++";
+
+//string title = "my_title');INSERT INTO subjects (title) VALUES('ADMIN LOOSER!!!";
+
+//try
+//{
+//    conn.Open();
+//    Console.WriteLine("Connection OK");
+
+//    string query = $@"INSERT INTO subjects (title) VALUES (@title)";
+//    Console.WriteLine(query);
+
+//    SqlCommand cmd = new SqlCommand(query, conn);
+
+//    SqlParameter prm = new SqlParameter("@title", title)
+//    {
+//        SqlDbType = SqlDbType.NVarChar,
+//        Size = 256
+//    };
+//    cmd.Parameters.Add(prm);
+
+//    cmd.ExecuteNonQuery();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"ERROR: {ex.Message}");
+//}
+//finally
+//{
+//    if (conn.State == System.Data.ConnectionState.Open)
+//    {
+//        conn.Close();
+//        Console.WriteLine("Connection closed");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using SqlConnection conn = new SqlConnection(connString);
+
+string title = "P42";
+int status = 3;
+
+try
+{
+    conn.Open();
+    Console.WriteLine("Connection OK");
+
+    string query = @"
+            INSERT INTO groups (title, status)
+            VALUES (@title, @status);
+            SET @last_id = SCOPE_IDENTITY();
+        ";
+
+    SqlCommand cmd = new SqlCommand(query, conn);
+
+    cmd.Parameters.Add(new SqlParameter("@title", title)
+    {
+        SqlDbType = SqlDbType.NVarChar,
+        Size = 128,
+    });
+
+    cmd.Parameters.Add(new SqlParameter("@status", status)
+    {
+        SqlDbType = SqlDbType.TinyInt,
+    });
+
+    SqlParameter idPrm = new SqlParameter()
+    {
+        ParameterName = "@last_id",
+        SqlDbType = SqlDbType.Int,
+        Direction = ParameterDirection.Output,
+    };
+    cmd.Parameters.Add(idPrm);
+
+    cmd.ExecuteNonQuery();
+
+    Console.WriteLine($"last_id = {idPrm.Value}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+finally
+{
+    if (conn.State == System.Data.ConnectionState.Open)
+    {
+        conn.Close();
+        Console.WriteLine("Connection closed");
+    }
+}
